cd2fe26437697221b247fde7784965f1f411dfa8
Zbigniew Skolicki <zskolicki@virtuslab.com>
2017-10-29 02:33:40 +0200

    external urls

--- bash/README.txt
+++ bash/README.txt
@@ -1,9 +1,12 @@
-Run ./test.sh to
+Run ./run.sh to
 - append key to dane in the first column
 - append key to slownik in the first column
 - join
 - generate pairable.txt with matched data
 - generate impairable.txt with the unmatched data
+- query openstreetmap for lat/long
+- query epsg.io for PL 2000 coords
+- save in fix.txt, join to final.txt
 
 Run ./analyze.sh to better see the unmached data
 
--- bash/test.sh
+++ bash/test.sh
@@ -1,16 +1,40 @@
+echo Starting time:
+date
 echo Preparing dane
-./dane.sh
+tail -n +2 "dane wejściowe.csv" \
+| awk -F\; '{print toupper($2" "$3)";"$0}' \
+| ./clean.sh  > dane.txt
+
 echo Preparing slownik
-./slownik.sh
+tail -n +2 "Słownik Adresów.csv" \
+| awk -F\; '{OFS=";";print toupper($1" "$2),$11,$12}' \
+| while read line; do
+    while [[ ! $line =~ ^[^\ ]*\;.* ]]; do
+      echo $line
+      line=${line#* }
+    done
+  done \
+| ./clean.sh > slownik.txt
 
 echo Joining pairable
-join -t";" -1 1 -2 1 dane2.txt slownik2.txt \
+join -t";" -1 1 -2 1 dane.txt slownik.txt \
 | tr -d '\r' \
+| cut -f 2- -d\; \
 > pairable.txt
 
 echo Joining unpairable
-join -t";" -1 1 -2 1 -v 1 dane2.txt slownik2.txt \
+join -t";" -1 1 -2 1 -v 1 dane.txt slownik.txt \
+| cut -f 2- -d\; \
 > impairable.txt
+
+./fix.sh <impairable.txt >fix.txt
+
+cat pairable.txt fix.txt | sort -k1 -t\; -n > final.txt
+
+echo Ending time
+date
 
 wc -l pairable.txt
 wc -l impairable.txt
+wc -l fix.txt
+wc -l final.txt
