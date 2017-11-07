b15cdcd8e3fbaa992e4914ffa34cf760ed2ecd61
mgolub <mgolub@virtuslab.com>
2017-11-07 03:45:22 +0100

    Added special cases to street name normalizing.

--- app/services/StringNormalizer.scala
+++ app/services/StringNormalizer.scala
@@ -8,7 +8,6 @@ object StringNormalizer {
 
   private val replacementTable = "ĄĆĘŁŃÓÖŚÜŹŻ".zip("ACELNOOSUZZ").toList
 
-  // FIXME There's a street called "OSIEDLE". Normalizing it leaves only whitespace
   private val thoroughfarePrefixes = List(
     "AL.",
     "ALEJA",
@@ -60,18 +59,53 @@ object StringNormalizer {
 
   def normalizeStreetName(input: String): String = {
     val normalized = normalize(input)
-    val withoutPrefixes = (thoroughfarePrefixes ++ titlePrefixes).foldLeft(normalized){
-      case (str, prefix) => str.replace(prefix, "")
-    }
-    val withoutInitials = withoutPrefixes
-      .replaceAll("""[A-Z]\.""", "")
-
-    val result = withoutInitials.trim
+    val result = normalizeSpecialCases.applyOrElse(normalized, normalizeGenericStreetName)
     logger.debug(s"Normalized '$input' to '$result'")
     if(result.length <= 3){
       logger.warn(s"Normalisation result is very short: '$input' to '$result'")
     }
     result
+  }
+
+  private def normalizeSpecialCases: PartialFunction[String, String] = {
+    // There's a street called "OSIEDLE". Literally
+    case "OSIEDLE" => "OSIEDLE"
+    // Another case where one of the standard prefixes is important
+    case "SWIETEGO KRZYZA" => "SWIETEGO KRZYZA"
+    // Parsing or spelling errors that repeat a few times
+    case x if x.contains("PETO;FIEGO") => "PETOFIEGO"
+    case x if x.contains("BRU;CKNERA") => "BRUCKNERA"
+    case x if x.contains("SZYSZKI-BOHUSZA") => "SZYSZKO-BOHUSZA"
+    case x if x.contains("MLYNSKA-BOCZNA") => "MLYNSKA BOCZNA"
+    // Some streets have shorter name forms in the dictionary than those occurring in the input data
+    case x if x.contains("RADZIKOWSKIEGO") => "ELJASZA RADZIKOWSKIEGO"
+    case x if x.contains("LELEWELA") => "BORELOWSKIEGO-LELEWELA"
+    case x if x.contains("JUNOSZY") => "KLEMENSA JUNOSZY SZANIAWSKIEGO"
+    case x if x.contains("LENARTOWICZA") => "LENARTOWICZA"
+    case x if x.contains("JAWORSKIEGO") => "JAWORSKIEGO"
+    case x if x.contains("MEISELSA") => "MEISELSA"
+    case x if x.contains("FELINSKIEGO") => "FELINSKIEGO"
+    case x if x.contains("WORONICZA") => "WORONICZA"
+    case x if x.contains("MONIUSZKI") => "MONIUSZKI"
+    case x if x.contains("SKRZETUSKIEGO") => "SKRZETUSKIEGO"
+    case x if x.contains("SEWERA") => "SEWERA"
+    case x if x.contains("CHODKIEWICZA") => "CHODKIEWICZA"
+    case x if x.contains("WARSZAUERA") => "WARSZAUERA"
+    case x if x.contains("SAPIEHY") => "SAPIEHY"
+    case x if x.contains("GIERYMSKICH") => "GIERYMSKICH"
+    case x if x.contains("WAWEL") => "WAWEL"
+    case x if x.contains("REYMONTA") => "REYMONTA"
+    case x if x.contains("ZWIRKI I WIGURY") => "ZWIRKI I WIGURY"
+  }
+
+  private def normalizeGenericStreetName(input: String): String = {
+    val withoutPrefixes = (thoroughfarePrefixes ++ titlePrefixes).foldLeft(input){
+      case (str, prefix) => str.replace(prefix, "")
+    }
+    val withoutInitials = withoutPrefixes
+      .replaceAll("""[A-Z]\.""", "")
+
+    withoutInitials.trim
   }
 
 }
